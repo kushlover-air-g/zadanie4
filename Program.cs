e1d6a7361b191752873a071f22036acea6432ed5
kushlover-air-g <59054712+kushlover-air-g@users.noreply.github.com>
2019-12-19 20:09:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dec, sot, ed, factordec=1, factored=1, factorsot=1;
+            for(int i = 100; i<1000;i++)
+            {
+                sot = i / 100;
+                dec = (i - (sot * 100)) / 10;
+                ed = (i - (sot * 100 + dec * 10));
+                for(int k =1; k<=sot; k++)
+                {
+                    factorsot *= k;
+                }
+                for (int j = 1; j <= dec; j++)
+                {
+                    factordec *= j;
+                }
+                for (int s = 1; s <= sot; s++)
+                {
+                    factored *= s;
+                }
+                if(factordec+factored+factorsot==i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
